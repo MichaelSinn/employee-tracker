ec9010ea06d4e5f44282fee7668c345eb9424c92
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-19 18:44:43 -0400

    Added seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,19 @@
+USE store_db;
+INSERT INTO department (name) VALUES ('Service');
+INSERT INTO department (name) VALUES ('Sales');
+INSERT INTO department (name) VALUES ('Accounting');
+INSERT INTO department (name) VALUES ('HR');
+
+INSERT INTO role (title, salary, department_id) VALUES ('Director', 100000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ('Salesman', 100000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ('Accountant', 100000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ('HR Employee', 100000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ('HR Manager', 100000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'Tinker', 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sally', 'Smith', 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Craig', 'Paul', 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Xander', 'Leszkowiat', 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Emily', 'Lumberjack', 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Nathan', 'Wrong', 4, 5);
+
