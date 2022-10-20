9a91b83a91d09e15188235f48a21042b2a3275ac
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-19 23:32:54 -0400

    Updated default salaries

--- db/seed.sql
+++ db/seed.sql
@@ -4,11 +4,11 @@ INSERT INTO department (name) VALUES ('Sales');
 INSERT INTO department (name) VALUES ('Accounting');
 INSERT INTO department (name) VALUES ('HR');
 
-INSERT INTO role (title, salary, department_id) VALUES ('Director', 100000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ('Salesman', 100000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ('Accountant', 100000, 3);
-INSERT INTO role (title, salary, department_id) VALUES ('HR Employee', 100000, 4);
-INSERT INTO role (title, salary, department_id) VALUES ('HR Manager', 100000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ('Director', 110000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ('Salesman', 70000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ('Accountant', 90000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ('HR Employee', 60000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ('HR Manager', 850000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'Tinker', 1, null);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sally', 'Smith', 3, null);
