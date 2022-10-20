ecf73f0e3c8fb1fbeb7901183729be38463a8948
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-19 22:37:03 -0400

    Completed acceptance criteria

--- db/schema.sql
+++ db/schema.sql
@@ -22,6 +22,6 @@ CREATE TABLE employee(
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- main.js
+++ main.js
@@ -84,6 +84,21 @@ const employeePrompt = [
     },
 ]
 
+const updateEmployeePrompt = [
+    {
+        type: "list",
+        name: "id",
+        message: "Choose an employee:",
+        choices: []
+    },
+    {
+        type: "list",
+        name: "role_id",
+        message: "Choose a role:",
+        choices: []
+    }
+]
+
 async function menu(){
     let continueLooping = true;
     while(continueLooping) {
@@ -91,31 +106,35 @@ async function menu(){
             const responses = await inquirer.prompt(menuPrompt);
             // const responses = {menu: "Add an Employee"};
             switch (responses["menu"]) {
-                case "View all Departments":
+                case "View all Departments": {
                     db.query("SELECT id, name as 'Department Name' FROM department", (err, results) => {
                         console.table("\nDepartments", results);
                     });
                     break;
-                case "View all Roles":
+                }
+                case "View all Roles": {
                     db.query("SELECT role.id AS id, role.title as Title, role.salary as Salary, department.name as Department FROM role JOIN department on role.department_id = department.id", (err, results) => {
                         console.table("\nRoles", results);
                     });
                     break;
-                case "View all Employees":
+                }
+                case "View all Employees": {
                     db.query("SELECT employee.id as id, employee.first_name as 'First Name', employee.last_name as 'Last Name', title as 'Title', salary as Salary, name as Department, CONCAT(e.first_name, ' ', e.last_name) as Manager FROM employee JOIN role r on employee.role_id = r.id JOIN department d on d.id = r.department_id LEFT JOIN employee e on employee.manager_id = e.id", (err, results) => {
                         console.table("\nEmployees", results);
                     });
                     break;
-                case "Add a Department":
+                }
+                case "Add a Department": {
                     const {newDepartment} = await inquirer.prompt(departmentPrompt);
                     db.query("INSERT INTO department (name) value (?)", newDepartment, (err) => {
                         err ? console.error(err) : true;
                     });
                     console.log(`Added department ${newDepartment}.`);
                     break;
-                case "Add a Role":
+                }
+                case "Add a Role": {
                     const departments = await qu.getDepartments();
-                    departments.forEach(department =>{
+                    departments.forEach(department => {
                         rolePrompt[2].choices.push({name: department.name, value: department.id})
                     });
                     const {roleName, salary, departmentId} = await inquirer.prompt(rolePrompt);
@@ -124,30 +143,41 @@ async function menu(){
                     });
                     console.log(`Added role ${roleName}.`);
                     break;
-                case "Add an Employee":
-                    // TODO: I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+                }
+                case "Add an Employee": {
                     const roles = await qu.getRoles();
-                    roles.forEach(r =>{
+                    roles.forEach(r => {
                         employeePrompt[2].choices.push({name: r.title, value: r.id});
                     });
                     const employees = await qu.getEmployeeNames();
-                    employees.forEach(emp =>{
+                    employees.forEach(emp => {
                         employeePrompt[3].choices.push({name: emp.Name, value: emp.id});
                     });
                     const {first_name, last_name, role, manager} = await inquirer.prompt(employeePrompt);
-                    db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [first_name, last_name, role, manager], (err, result)=>{
+                    db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [first_name, last_name, role, manager], (err, result) => {
                         err ? console.error(err) : true;
                     });
                     break;
-                case "Update an Employee Role":
-                    // TODO: I am prompted to select an employee to update and their new role and this information is updated in the database
-                    db.query("", (err, result)=>{
-
+                }
+                case "Update an Employee Role": {// TODO: I am prompted to select an employee to update and their new role and this information is updated in the database
+                    const employees = await qu.getEmployeeNames();
+                    employees.forEach(emp =>{
+                        updateEmployeePrompt[0].choices.push({name: emp.Name, value: emp.id});
+                    });
+                    const roles = await qu.getRoles();
+                    roles.forEach(role =>{
+                        updateEmployeePrompt[1].choices.push({name: role.title, value: role.id});
+                    });
+                    const {id, role_id} = await inquirer.prompt(updateEmployeePrompt);
+                    db.query("UPDATE employee SET role_id = ? WHERE id = ?", [role_id, id], (err, result)=>{
+                        err ? console.error(err) : true;
                     });
                     break;
-                case "Quit":
+                }
+                case "Quit": {
                     continueLooping = false;
                     break;
+                }
             }
         } catch (err) {
             console.error(`Error: ${err}`);
